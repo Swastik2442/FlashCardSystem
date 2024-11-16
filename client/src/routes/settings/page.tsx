63bc897c35386c91673b8bc68e56b91aa17fb83d
Swastik Kulshreshtha <56737344+Swastik2442@users.noreply.github.com>
2024-11-16 13:33:57 +0530

    Add Sidebar to Settings Page

--- client/src/routes/settings/page.tsx
+++ client/src/routes/settings/page.tsx
@@ -1,10 +1,41 @@
 import { JSX } from "react";
-import { useLoaderData, useNavigate, LoaderFunctionArgs } from "react-router-dom";
+import { useLoaderData, Link, LoaderFunctionArgs } from "react-router-dom";
+import { SettingsIcon, ShieldCheck, User } from "lucide-react";
 import { useAuth } from "@/contexts/authProvider";
+import { SidebarProvider, SidebarTrigger, Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { UserOptions, UserOptionsLoader } from "./userOptions";
 import { AccountOptions, AccountOptionsLoader } from "./accountOptions";
 import { SecurityOptions } from "./securityOptions";
+import { useMediaQuery } from "@/hooks/mediaQuery";
+
+const settingsOptions = [
+  {
+    label: null,
+    items: [
+      {
+        title: "Profile",
+        url: "/settings/user",
+        icon: User,
+      },
+      {
+        title: "Account",
+        url: "/settings/account",
+        icon: SettingsIcon,
+      },
+    ]
+  },
+  {
+    label: "Access",
+    items: [
+      {
+        title: "Authentication",
+        url: "/settings/security",
+        icon: ShieldCheck,
+      },
+    ]
+  },
+] as const;
 
 export async function SettingsLoader({ params }: LoaderFunctionArgs) {
   let name = params.name, Component, data;
@@ -35,21 +66,65 @@ export async function SettingsLoader({ params }: LoaderFunctionArgs) {
 }
 
 export function Settings() {
-  const { name, Component, data } = useLoaderData() as { name: string, Component: ({ data }: { data?: unknown }) => JSX.Element, data: unknown };
-  const navigate = useNavigate();
+  const { name, Component, data } = useLoaderData() as {
+    name: string,
+    Component: ({ data }: { data?: unknown }) => JSX.Element,
+    data: unknown
+  };
   const { user } = useAuth();
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return (
     <div className="my-4">
-      <div className="flex justify-between ml-10 mr-4">
-        <h1 className="text-lg select-none">{name} Settings</h1>
-        <Button onClick={() => navigate(`/users/${user}`)} type="button" title="Profile" variant="outline" size="sm">Your Profile</Button>
-      </div>
-      <hr className="my-4" />
-      <div className="mx-8">
-        <Component data={data} />
-      </div>
+      <SidebarProvider>
+        <SettingsSidebar collapsible={!isDesktop} />
+        <div className="h-full w-full">
+          <div className="flex justify-between mr-4 ml-2 md:ml-4">
+            <div className="flex gap-1 items-center">
+              {!isDesktop && <SidebarTrigger />}
+              <h1 className="text-lg select-none">{name} Settings</h1>
+            </div>
+            <Link to={`/users/${user}`}>
+              <Button type="button" title="Profile" variant="outline" size="sm">
+                Your Profile
+              </Button>
+            </Link>
+          </div>
+          <hr className="my-4" />
+          <div className="mx-4">
+            <Component data={data} />
+          </div>
+        </div>
+      </SidebarProvider>
     </div>
+  );
+}
+
+function SettingsSidebar({ collapsible }: { collapsible: boolean }) {
+  return (
+    <Sidebar collapsible={collapsible ? "offcanvas" : "none"}>
+      <SidebarContent>
+        {settingsOptions.map((group) => (
+          <SidebarGroup>
+            {group.label && <SidebarGroupLabel>{group.label}</SidebarGroupLabel>}
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link to={item.url} preventScrollReset={true}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+    </Sidebar>
   );
 }
 
